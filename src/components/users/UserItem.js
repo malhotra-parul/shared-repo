bf226bf406194ee99656d64507d7963fbf909ab1
ip11 <iprash011@gmail.com>
2020-03-29 15:42:02 +0530

    Link to UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Link} from 'react-router-dom';
 
 const UserItem = ({user: {login, avatar_url, html_url}})=>{
     return(
@@ -10,10 +11,10 @@ const UserItem = ({user: {login, avatar_url, html_url}})=>{
                     {login}
                 </h3>
                 <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">
+                    <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
                         {' '}
                         Git Profile
-                    </a>
+                    </Link>
                 </div>
             </div>
     );
