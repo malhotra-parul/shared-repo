87d1969313d6814046482f4cea87189af6b482c3
ip11 <iprash011@gmail.com>
2020-03-29 16:53:26 +0530

    img and location conditional rendering

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Spinner from '../layouts/Spinner';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
@@ -37,6 +37,19 @@ class User extends Component {
 				<Link to="/" className="btn btn-light">
 					Back to Search Page
 				</Link>
+				Hireable :{' '}
+				{hireable ? (
+					<i className="fas fa-check text-success" />
+				) : (
+					<i className="fas fa-times-circle text-danger" />
+				)}
+				<div className="card grid-2">
+					<div className="all-center">
+						<img src={avatar_url} className="round-img" alt="Git Image" style={{ width: '150px' }} />
+						<h1>{name}</h1>
+						<p>Location : {location} </p>
+					</div>
+				</div>
 			</Fragment>
 		);
 	}
